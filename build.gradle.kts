dd86ecd84fe5d8eeca142f7de00218a4ff3d6eae
Dima <dimabarnevdb@gmail.com>
2026-05-30 20:43:52 +0300

    change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ spotless {
 }
 
 // ---- Release version — edit here ----
-val baseVersionCode = 42183
+val baseVersionCode = 42185
 
 @Suppress("DEPRECATION")
 android {
@@ -39,13 +39,13 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = baseVersionCode
-        versionName = "1.1.1+2.20.0"
+        versionName = "1.5.3+2.20.0"
         applicationId = "com.dimax.impulse"
         resValue("string", "applicationId", applicationId!!)
         val appName = "Impulse"
         buildConfigField("String", "APP_NAME", "\"$appName\"")
         base {
-            archivesName.set("com.dimax.impulse-1.0.0+2.20.0")
+            archivesName.set("com.dimax.impulse-1.5.3+2.20.0")
         }
     }
 
