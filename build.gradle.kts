b7252f9b826971f6c935d99bcb91f37b63fe6da1
Dima <dimabarnevdb@gmail.com>
2026-06-14 10:42:02 +0300

    Bump version to: Name = 1.11.0-alpha.9 Code = 42211

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ spotless {
 }
 
 // ---- Release version — edit here ----
-val baseVersionCode = 42210
+val baseVersionCode = 42211
 
 @Suppress("DEPRECATION")
 android {
@@ -39,13 +39,13 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = baseVersionCode
-        versionName = "1.11.0-alpha.8+2.20.0"
+        versionName = "1.11.0-alpha.9+2.20.0"
         applicationId = "com.dimax.impulse"
         resValue("string", "applicationId", applicationId!!)
         val appName = "Impulse"
         buildConfigField("String", "APP_NAME", "\"$appName\"")
         base {
-            archivesName.set("com.dimax.impulse_1.11.0-alpha.8" +
+            archivesName.set("com.dimax.impulse_1.11.0-alpha.9" +
                     "+2.20.0")
         }
     }
